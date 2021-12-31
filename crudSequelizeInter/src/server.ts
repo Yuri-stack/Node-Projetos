b4f1dbaf448426e10a9ae7203d3644ce9eca1a2a
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-12-31 18:33:36 -0300

    Config.: Configurando a Estrutura do Sequelize

--- crudSequelizeInter/src/server.ts
+++ crudSequelizeInter/src/server.ts
@@ -1,8 +1,14 @@
 import express from "express"
+import { router } from "./routes"
+const sequelize = require("./config/dbConfig")
 
 const app = express()
 
-app.listen(3000, () => {
+app.use(express.json())
+app.use(router)
+
+app.listen(3000, async () => {
+    await sequelize.sync()
     console.log("Server is running, my friend")
 })
 
