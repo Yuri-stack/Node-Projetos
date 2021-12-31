7a4bed2de0f5fe55d1b43ced7366ed3c44b4b1a9
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-12-31 19:22:11 -0300

    Feat. Criando Banco de Dados e Tabela Users com Migration

--- crudSequelizeInter/src/config/dbConfig.ts
+++ crudSequelizeInter/src/config/dbConfig.ts
@@ -1,21 +1,37 @@
-const { Sequelize } = require("sequelize")
 require('dotenv').config()
 
-// Opção 3 de Config.: Passando parametros separadamentes
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, 
-    {
-        // host: process.env.DB_HOST,
-        dialect: "mysql", // Tipo de Banco de Dados
-        port: +process.env.DB_PORT,  // (+) Casting, de String para Number
-
-        define: {
-            timestamps: true,   // Toda tabela tem um campo createAt e updateAt
-            underscored: true   // Defini os nomes das tabelas/colunas em Snake Case(user_group)
-        }
+module.exports = {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: {
+        timestamps: true,   // Toda tabela tem um campo createAt e updateAt
+        underscored: true   // Defini os nomes das tabelas/colunas em Snake Case(user_group)
     }
-)
+}
 
-module.exports = sequelize
+// A configuração abaixo é usada em casos que você não irá usar Migrations
+
+// const { Sequelize } = require("sequelize")
+// require('dotenv').config()
+
+// // Opção 3 de Config.: Passando parametros separadamente
+// const sequelize = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD, 
+//     {
+//         // host: process.env.DB_HOST,
+//         dialect: "mysql", // Tipo de Banco de Dados
+//         port: +process.env.DB_PORT,  // (+) Casting, de String para Number
+
+//         define: {
+//             timestamps: true,   // Toda tabela tem um campo createAt e updateAt
+//             underscored: true   // Defini os nomes das tabelas/colunas em Snake Case(user_group)
+//         }
+//     }
+// )
+
+// module.exports = sequelize
