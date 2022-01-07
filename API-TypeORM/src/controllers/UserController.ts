d72cf424243c0accf7cc9ac4b2b589f703dd652a
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2022-01-07 15:48:02 -0300

    Feat.: Add Services

--- API-TypeORM/src/controllers/UserController.ts
+++ API-TypeORM/src/controllers/UserController.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express"
+import { UserService } from "../services/UserService"
 
 const UserController = {
     
     findAll(req: Request, res: Response){
         return res.json({ messagem: "Usuarios"})
+    },
+
+    create(req: Request, res: Response){
+        const { name, email } = req.body
+
+        if(name.length === 0) return res.status(400).json({ Messagem: "Verifique os campos" })
+
+        const user = UserService.execute({ name, email })
+
+        return res.json(user)
     }
 }
 
--- API-TypeORM/src/routes.ts
+++ API-TypeORM/src/routes.ts
@@ -4,6 +4,7 @@ import { UserController } from "./controllers/UserController"
 const routes = express.Router()
 
 routes.get("/users", UserController.findAll)
+routes.post("/users", UserController.create)
 
 export { routes }
 
