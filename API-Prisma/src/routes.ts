d290791b761ab95fba87a9fbbfe1fb4c4b7ae897
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-02-19 14:52:43 -0300

    Feat.: Add Controllers de Produto com Categoria existente

--- API-Prisma/src/routes.ts
+++ API-Prisma/src/routes.ts
@@ -2,15 +2,18 @@ import { Router } from 'express'
 import { CreateCategoryController } from './controllers/CreateCategoryController'
 import { CreateProductCategoryController } from './controllers/CreateProductCategoryController'
 import { CreateProductController } from './controllers/CreateProductController'
+import { CreateProductWithExistCategory } from './controllers/CreateProductWithExistCategory'
 
 const router = Router()
 
-const createProduct = new CreateProductController();
-const createCategory = new CreateCategoryController();
-const createProductCategory = new CreateProductCategoryController();
+const createProduct = new CreateProductController()
+const createCategory = new CreateCategoryController()
+const createProductCategory = new CreateProductCategoryController()
+const createProductCategoryExists = new CreateProductWithExistCategory()
 
 router.post("/product", createProduct.handle)
 router.post("/category", createCategory.handle)
 router.post("/categoryProduct", createProductCategory.handle);
+router.post("/productWithCategory", createProductCategoryExists.handle);
 
 export { router }
